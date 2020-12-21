42e424c65748987b8c0b908200af29a18f540c2e
Alesh <xryska01@stud.feec.vutbr.cz>
2020-12-21 13:57:56 +0100

    MarsApiService.kt edit

--- MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ MarsRealEstate-Starter/app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -17,4 +17,26 @@
 
 package com.example.android.marsrealestate.network
 
+import retrofit2.Retrofit
+import retrofit2.http.GET
+
 private const val BASE_URL = "https://android-kotlin-fun-mars-server.appspot.com/"
+
+private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+private val retrofit = Retrofit.Builder()
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .baseUrl(BASE_URL)
+        .build()
+
+interface MarsApiService {
+    @GET("realestate")
+    suspend fun getProperties(): List<MarsProperty>
+}
+
+object MarsApi {
+    val retrofitService : MarsApiService by lazy {
+        retrofit.create(MarsApiService::class.java) }
+}
